59527dc56cfe43cf6c60e1a9065abb335459ae6d
MinChang <rud0806@gmail.com>
2021-11-01 21:27:09 +0900

    [Feat] Init RecordingView

---
+++ SanTa/SanTa/RecordingScene/RecordingViewController+Extension.swift
@@ -0,0 +1,12 @@
+//
+//  RecordingViewController+Extension.swift
+//  SanTa
+//
+//  Created by 김민창 on 2021/11/01.
+//
+
+import UIKit
+
+extension RecordingViewController {
+    
+}
--- SanTa/SanTa/RecordingScene/RecordingViewController.swift
+++ SanTa/SanTa/RecordingScene/RecordingViewController.swift
@@ -9,9 +9,95 @@ import UIKit
 
 class RecordingViewController: UIViewController {
     weak var coordinator: RecordingViewCoordinator?
+    
+    let kilometerLabel: UILabel = {
+        let label = UILabel()
+        label.font = .preferredFont(forTextStyle: .headline)
+        label.text = "0.00"
+        label.textColor = .white
+        return label
+    }()
+    
+    let kilometerTextLabel: UILabel = {
+        let label = UILabel()
+        label.font = .preferredFont(forTextStyle: .title3)
+        label.text = "킬로미터"
+        label.textColor = .white
+        return label
+    }()
+    
+    let timeLabel: UILabel = {
+        let label = UILabel()
+        label.font = .preferredFont(forTextStyle: .body)
+        label.text = "00:00 00\""
+        label.textColor = .white
+        return label
+    }()
+    
+    let altitudeLabel: UILabel = {
+        let label = UILabel()
+        label.font = .preferredFont(forTextStyle: .body)
+        label.text = "0"
+        label.textColor = .white
+        return label
+    }()
+    
+    let walkLabel: UILabel = {
+        let label = UILabel()
+        label.font = .preferredFont(forTextStyle: .body)
+        label.text = "0"
+        label.textColor = .white
+        return label
+    }()
+    
+    let timeTextLabel: UILabel = {
+        let label = UILabel()
+        label.font = .preferredFont(forTextStyle: .body)
+        label.text = "시간"
+        label.textColor = .white
+        return label
+    }()
+    
+    let altitudeTextLabel: UILabel = {
+        let label = UILabel()
+        label.font = .preferredFont(forTextStyle: .body)
+        label.text = "고도"
+        label.textColor = .white
+        return label
+    }()
+    
+    let walkTextLabel: UILabel = {
+        let label = UILabel()
+        label.font = .preferredFont(forTextStyle: .body)
+        label.text = "걸음"
+        label.textColor = .white
+        return label
+    }()
+    
+    
+    let calculateStackView: UIStackView = {
+        let stackView = UIStackView()
+        stackView.translatesAutoresizingMaskIntoConstraints = false
+        stackView.axis = .horizontal
+        stackView.spacing = 10
+        stackView.distribution = .fillEqually
+
+        return stackView
+    }()
+    
+    
+    let calculateTextStackView: UIStackView = {
+        let stackView = UIStackView()
+        stackView.translatesAutoresizingMaskIntoConstraints = false
+        stackView.axis = .horizontal
+        stackView.spacing = 10
+        stackView.distribution = .fillEqually
+
+        return stackView
+    }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = .blue
+        self.configureViews()
     }
 }
