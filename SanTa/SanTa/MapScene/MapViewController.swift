03953b717865a09a291b760089b51f3f27713ae8
Shin <struct.developer@gmail.com>
2021-11-02 11:03:07 +0900

    [Feat] MapView Layout 및 마커 구현
    Alert을 이용해 사용자로 부터 위치 정보를 활성화할 수 있게 권한을 획득한다.
    MapKit을 사용하여 화면에 지도를 표시한다.
    위치 서비스가 꺼져 있다면 대한민국의 전체를 보여줄 수 있게 축척 및 위치를 조절한다.
    위치 서비스가 켜져 있다면 현재 위치가 보이도록 축척 및 위치를 조절한다.
    MapKit 클러스터링 기능을 활용한다.
    44x44 크기 이내에 존재하여 겹치는 마커들은 중간위치에 숫자로 표시한다.

--- SanTa/SanTa/MapScene/MapViewController.swift
+++ SanTa/SanTa/MapScene/MapViewController.swift
@@ -4,15 +4,85 @@
 //
 //  Created by shin jae ung on 2021/10/28.
 //
-
-import UIKit
+import MapKit
 
 class MapViewController: UIViewController {
     weak var coordinator: MapViewCoordinator?
+    private var mapView = MKMapView()
+    private var manager: CLLocationManager?
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = .red
+        self.configureViews()
+        self.registerAnnotationView()
+        self.configureCoreLocationManager()
+        self.testCoordinates()
+    }
+    
+    private func configureViews() {
+        self.mapView = MKMapView(frame: view.bounds)
+        self.mapView.showsUserLocation = true
+        self.view.addSubview(self.mapView)
+    }
+    
+    private func registerAnnotationView() {
+        self.mapView.register(
+            MountainAnnotationView.self,
+            forAnnotationViewWithReuseIdentifier: MKMapViewDefaultAnnotationViewReuseIdentifier
+        )
+        self.mapView.register(
+            ClusterAnnotationView.self,
+            forAnnotationViewWithReuseIdentifier: MKMapViewDefaultClusterAnnotationViewReuseIdentifier
+        )
+    }
+    
+    private func configureCoreLocationManager() {
+        self.manager = CLLocationManager()
+        self.manager?.requestWhenInUseAuthorization()
+        self.manager?.requestAlwaysAuthorization()
+        self.manager?.delegate = self
+        self.manager?.desiredAccuracy = kCLLocationAccuracyBest
+        self.manager?.startUpdatingLocation()
+    }
+    
+    private func render(_ location: CLLocation) {
+        let coordinate = CLLocationCoordinate2D(
+            latitude: location.coordinate.latitude,
+            longitude: location.coordinate.longitude
+        )
+        let span = MKCoordinateSpan(
+            latitudeDelta: 0.01,
+            longitudeDelta: 0.01
+        )
+        let region = MKCoordinateRegion(center: coordinate, span: span)
+        self.mapView.setRegion(region, animated: true)
+    }
+    
+    private func testCoordinates() {
+        for _ in 0...200 {
+            let latitude = Double.random(in: 36.0..<37.0)
+            let longitude = -Double.random(in: 121.0..<122.0)
+            let pin = MKPointAnnotation()
+            pin.coordinate = CLLocationCoordinate2D(latitude: latitude, longitude: longitude)
+            mapView.addAnnotation(pin)
+        }
     }
 }
 
+extension MapViewController: MKMapViewDelegate {
+    func mapView(_ mapView: MKMapView, viewFor annotation: MKAnnotation) -> MKAnnotationView? {
+        return MountainAnnotationView(
+            annotation: annotation,
+            reuseIdentifier: MountainAnnotationView.ReuseID
+        )
+    }
+}
+
+extension MapViewController: CLLocationManagerDelegate {
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        if let location = locations.first {
+            manager.stopUpdatingLocation()
+            self.render(location)
+        }
+    }
+}
