8ac861a89c7d62fab8d280e4f334934da82bb5ca
MinChang <rud0806@gmail.com>
2021-11-04 16:28:44 +0900

    [Fix] RecordingUseCase Combine 프로퍼티 삭제

--- SanTa/SanTa/RecordingScene/RecordingUseCase.swift
+++ SanTa/SanTa/RecordingScene/RecordingUseCase.swift
@@ -6,28 +6,17 @@
 //
 
 import Foundation
-import Combine
 
 protocol RecordingUseCase {
-    var currentTime: String { get set }
-    var kilometer: String  { get set }
-    var altitude: String  { get set }
-    var walk: String  { get set }
     var recording: RecordingModel { get set }
     
     func save(completion: @escaping (Result<Record, Error>) -> Void)
 }
 
 final class DefaultRecordingUseCase: RecordingUseCase, ObservableObject {
-    @Published var currentTime = ""
-    @Published var kilometer = ""
-    @Published var altitude = ""
-    @Published var walk = ""
-    
     var recording: RecordingModel
     
     private let recordRepository: RecordRepository
-    private var subscriptions = Set<AnyCancellable>()
     
     init(recordRepository: RecordRepository, recordingModel: RecordingModel) {
         self.recordRepository = recordRepository
