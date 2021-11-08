5f08a3fdd041902d8f20f7f263ca3c38122aae58
OHCHANGMIN <lou0124@naver.com>
2021-11-08 16:12:41 +0900

    [Feat] #74 설정화면 Retositoy 구현

--- SanTa/SanTa/SettingsScene/SettingsRepository.swift
+++ SanTa/SanTa/SettingsScene/SettingsRepository.swift
@@ -6,3 +6,51 @@
 //
 
 import Foundation
+
+protocol SettingsRepository {
+    func save<T: Codable>(value: T, key: Settings)
+    func getToggleOption(key: Settings, completion: @escaping (Option) -> Void)
+    func getMapOption(key: Settings, completion: @escaping (Option) -> Void)
+}
+
+final class DefaultSettingsRepository: SettingsRepository {
+    
+    let settingsStorage: SettingsStorage
+    
+    init(settingsStorage: SettingsStorage) {
+        self.settingsStorage = settingsStorage
+    }
+    
+    func save<T: Codable>(value: T, key: Settings) {
+        self.settingsStorage.save(value: value, key: key)
+    }
+    
+    func getToggleOption(key: Settings, completion: @escaping (Option) -> Void) {
+        self.settingsStorage.exist(key: key) { exist in
+            if !exist {
+                guard let value = key.initValue as? Bool else { return }
+                self.settingsStorage.save(value: value, key: key)
+            }
+            self.settingsStorage.bool(key: key) { value in
+                let option = ToggleOption(text: key.title, toggle: value)
+                completion(option)
+            }
+        }
+    }
+    
+    func getMapOption(key: Settings, completion: @escaping (Option) -> Void) {
+        self.settingsStorage.exist(key: key) { exist in
+            if !exist {
+                guard let value = key.initValue as? String else { return }
+                self.settingsStorage.save(value: value, key: key)
+            }
+            self.settingsStorage.string(key: key) { value in
+                guard let value = value else { return }
+                guard let map = Map.init(rawValue: value) else { return }
+                let option = MapOption(text: key.title, map: map)
+                completion(option)
+            }
+        }
+    }
+}
+
--- SanTa/SanTa/SettingsScene/UserDefaultsSettingsStorage.swift
+++ SanTa/SanTa/SettingsScene/UserDefaultsSettingsStorage.swift
@@ -9,9 +9,9 @@ import Foundation
 
 protocol SettingsStorage {
     func save<T: Codable>(value: T, key: Settings)
-    func exist(key: Settings) -> Bool
-    func bool(key: Settings) -> Bool
-    func string(key: Settings) -> String?
+    func exist(key: Settings, completion: @escaping (Bool) -> Void)
+    func bool(key: Settings, completion: @escaping (Bool) -> Void)
+    func string(key: Settings, completion: @escaping (String?) -> Void)
 }
 
 final class UserDefaultsSettingsStorage: SettingsStorage {
@@ -26,30 +26,15 @@ final class UserDefaultsSettingsStorage: SettingsStorage {
         self.userDefaults.set(value, forKey: key.title)
     }
     
-    func exist(key: Settings) -> Bool {
-        return self.userDefaults.object(forKey: key.title) != nil
+    func exist(key: Settings, completion: @escaping (Bool) -> Void) {
+        completion(self.userDefaults.object(forKey: key.title) != nil)
     }
     
-    func bool(key: Settings) -> Bool {
-        return self.userDefaults.bool(forKey: key.title)
+    func bool(key: Settings, completion: @escaping (Bool) -> Void) {
+        completion(self.userDefaults.bool(forKey: key.title))
     }
     
-    func string(key: Settings) -> String? {
-        return self.userDefaults.string(forKey: key.title)
-    }
-}
-
-
-enum Settings: String {
-    case recordPhoto = "사진 기록하기"
-    case photosOnMap = "지도에 사진표시"
-    case autoPauseResume = "자동 일시정지, 재시작"
-    case autoPauseResumeVoiceGuidance = "자동 일시정지, 재시작 음성 안내"
-    case voiceGuidanceEveryOnekm = "1킬로미터 마다 음성 안내"
-    case mapFormat = "지도 형식"
-    case markHikingTrailsOnTheMap = "지도에 등산로 표시"
-    
-    var title: String {
-        return self.rawValue
+    func string(key: Settings, completion: @escaping (String?) -> Void) {
+        completion(self.userDefaults.string(forKey: key.title))
     }
 }
