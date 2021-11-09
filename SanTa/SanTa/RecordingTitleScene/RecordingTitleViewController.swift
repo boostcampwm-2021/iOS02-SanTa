766aa2f4e2d591e2b9dd3b95a960b096c12fa7d8
MinChang <rud0806@gmail.com>
2021-11-09 16:21:13 +0900

    [Feat] #105 RecoringTitleView Button Action 로직 추가

--- SanTa/SanTa/RecordingTitleScene/RecordingTitleViewController.swift
+++ SanTa/SanTa/RecordingTitleScene/RecordingTitleViewController.swift
@@ -140,11 +140,16 @@ class RecordingTitleViewController: UIViewController {
     }
     
     @objc private func inputButtonAction(sender: UIButton) {
+        guard let title = self.recordingTitleText.text else {
+            self.delegate?.didTitleWriteDone(title: "")
+            return
+        }
         
+        self.delegate?.didTitleWriteDone(title: title)
     }
     
     @objc private func notInputButtonButtonAction(sender: UIButton) {
-        
+        self.delegate?.didTitleWriteDone(title: "")
     }
 
 }
