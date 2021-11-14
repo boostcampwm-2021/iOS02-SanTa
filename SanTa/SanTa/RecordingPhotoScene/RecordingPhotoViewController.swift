b04cca4a2e058de64edd136b56a5bc3b5aadc188
MinChang <rud0806@gmail.com>
2021-11-14 18:52:05 +0900

    [Feat] #158 Coordinator, ViewController 생성 및 Coordinator 로직 추가

---
+++ SanTa/SanTa/RecordingPhotoScene/RecordingPhotoViewController.swift
@@ -0,0 +1,18 @@
+//
+//  RecordingPhotoViewController.swift
+//  SanTa
+//
+//  Created by 김민창 on 2021/11/14.
+//
+
+import UIKit
+
+class RecordingPhotoViewController: UIViewController {
+    weak var coordinator: RecordingPhotoViewCoordinator?
+    weak var delegate: RecordingViewDelegate?
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+}
