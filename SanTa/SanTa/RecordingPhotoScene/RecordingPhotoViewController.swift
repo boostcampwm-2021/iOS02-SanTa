5b0559eb8b2e0ff2e0cb368c61348dbd562b8a8b
MinChang <rud0806@gmail.com>
2021-11-15 16:12:44 +0900

    [Feat] #158 제어 레이아웃 수정

--- SanTa/SanTa/RecordingPhotoScene/RecordingPhotoViewController.swift
+++ SanTa/SanTa/RecordingPhotoScene/RecordingPhotoViewController.swift
@@ -22,6 +22,7 @@ class RecordingPhotoViewController: UIViewController {
     private let recordingPhotoImage: UIImageView = {
         let image = UIImageView()
         image.image = UIImage(systemName: "camera.fill")
+        image.tintColor = .systemBlue
         image.translatesAutoresizingMaskIntoConstraints = false
         return image
     }()
@@ -29,7 +30,8 @@ class RecordingPhotoViewController: UIViewController {
     private let recordingPhotoTitle: UILabel = {
         let label = UILabel()
         label.text = "사진 기록하기"
-        label.font = .preferredFont(forTextStyle: .headline)
+        label.font = .preferredFont(forTextStyle: .title1)
+        label.textAlignment = .center
         label.textColor = .systemGray
         label.translatesAutoresizingMaskIntoConstraints = false
         return label
@@ -38,7 +40,9 @@ class RecordingPhotoViewController: UIViewController {
     private let recordingPhotoDescription: UILabel = {
         let label = UILabel()
         label.text = "사진을 찍으면 지도에 표시됩니다."
-        label.font = .preferredFont(forTextStyle: .body)
+        label.font = .preferredFont(forTextStyle: .title2)
+        label.numberOfLines = 2
+        label.textAlignment = .center
         label.translatesAutoresizingMaskIntoConstraints = false
         return label
     }()
@@ -56,9 +60,9 @@ class RecordingPhotoViewController: UIViewController {
     
     private let photoStackView: UIStackView = {
         let stackView = UIStackView()
-        stackView.spacing = 12
+        stackView.spacing = 8
         stackView.axis = .vertical
-        stackView.distribution = .fillEqually
+        stackView.distribution = .fillProportionally
         stackView.alignment = .center
         stackView.translatesAutoresizingMaskIntoConstraints = false
         return stackView
@@ -74,7 +78,7 @@ class RecordingPhotoViewController: UIViewController {
         let frameWidth = view.frame.width
         let frameHeight = view.frame.height
         
-        self.displayView.frame = CGRect(x: 6, y: frameHeight - frameHeight/2, width: frameWidth - 12, height: frameHeight/3)
+        self.displayView.frame = CGRect(x: 6, y: frameWidth, width: frameWidth - 12, height: frameHeight/2 - 64)
         
         [self.recordingPhotoTitle, self.recordingPhotoImage, self.recordingPhotoDescription, self.agreeButton].forEach {
             self.photoStackView.addArrangedSubview($0)
@@ -86,15 +90,16 @@ class RecordingPhotoViewController: UIViewController {
         let recordingPhotoTitleConstraints = [
             self.recordingPhotoTitle.widthAnchor.constraint(equalToConstant: frameWidth - 12)
         ]
-        
+
         let recordingPhotoImageConstraints = [
-            self.recordingPhotoImage.widthAnchor.constraint(equalToConstant: frameWidth - 12)
+            self.recordingPhotoImage.widthAnchor.constraint(equalToConstant: frameWidth/3),
+            self.recordingPhotoImage.heightAnchor.constraint(equalTo: self.recordingPhotoImage.widthAnchor, constant: -16)
         ]
-        
+
         let recordingPhotoDescriptionText = [
             self.recordingPhotoDescription.widthAnchor.constraint(equalToConstant: frameWidth - 12)
         ]
-        
+
         let agreeButtonConstraints = [
             self.agreeButton.widthAnchor.constraint(equalToConstant: frameWidth - 12)
         ]
