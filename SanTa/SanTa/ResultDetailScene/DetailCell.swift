9078699a8852e6e54f446f31158143aaaf6a3ac3
Jiwon Yoon <>
2021-11-18 00:12:43 +0900

    [Feat] 기록 상세정보 셀 레이아웃

--- SanTa/SanTa/ResultDetailScene/DetailCell.swift
+++ SanTa/SanTa/ResultDetailScene/DetailCell.swift
@@ -8,22 +8,54 @@
 import UIKit
 
 class DetailCell: UICollectionViewCell {
-    var data: ResultDetailCellRepresentable?
-    let title: UILabel = UILabel()
+    private let title: UILabel = UILabel()
     
-    func configure(with cellData: ResultDetailCellRepresentable) {
-        self.data = cellData
+    func layout(data: ResultDetailCellRepresentable) {
+        self.title.text = data.title
+        self.title.textColor = .init(named: "SantaColor")
         
+        self.title.translatesAutoresizingMaskIntoConstraints = false
+        let titleConstraints = [
+            self.title.topAnchor.constraint(equalTo: self.topAnchor),
+            self.title.leftAnchor.constraint(equalTo: self.leftAnchor)
+        ]
+        NSLayoutConstraint.activate(titleConstraints)
         
+        var contentLabels: [UILabel] = []
+        var contentTitleLabels: [UILabel] = []
+        
+        for content in data.contents {
+            let contentLabel = UILabel()
+            contentLabel.text = content.content
+            let contentTitleLabel = UILabel()
+            contentTitleLabel.text = content.contentTitle
+            contentLabels.append(contentLabel)
+            contentTitleLabels.append(contentTitleLabel)
+        }
+        
+        let stack = UIStackView()
+        stack.axis = .vertical
+        
+        for index in 0..<data.contents.count {
+            let horizontalStack = UIStackView()
+            horizontalStack.axis = .horizontal
+            horizontalStack.addArrangedSubview(contentLabels[index])
+            horizontalStack.addArrangedSubview(contentTitleLabels[index])
+            stack.addArrangedSubview(horizontalStack)
+        }
+        
+        stack.translatesAutoresizingMaskIntoConstraints = false
+        let stackConstraint = [
+            stack.topAnchor.constraint(equalTo: self.title.bottomAnchor),
+            stack.leftAnchor.constraint(equalTo: self.leftAnchor),
+            stack.rightAnchor.constraint(equalTo: self.rightAnchor),
+            stack.bottomAnchor.constraint(equalTo: self.bottomAnchor)
+        ]
+        NSLayoutConstraint.activate(stackConstraint)
     }
 }
 
 extension DetailCell {
-    private func layout(data: ResultDetailCellRepresentable) {
-        let title = UILabel()
-        title.text = data.title
-    }
-    
     override func draw(_ rect: CGRect) {
         let path = UIBezierPath()
         path.lineWidth = 1
