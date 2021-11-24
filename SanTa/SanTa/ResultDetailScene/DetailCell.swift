067180e8fa19993cabf8a3f58ccba5e7772abe81
Jiwon Yoon <jiwonyoon@Jiwonui-MacBookAir.local>
2021-11-25 00:33:43 +0900

    [Refactor] #302 상세 정보 셀 리팩토링

--- SanTa/SanTa/ResultDetailScene/DetailCell.swift
+++ SanTa/SanTa/ResultDetailScene/DetailCell.swift
@@ -10,25 +10,39 @@ import UIKit
 class DetailCell: UICollectionViewCell {
     static let identifier = "DetailCell"
     
+    let title: UILabel = {
+        let label = UILabel()
+        label.font = .preferredFont(for: .body, weight: .bold)
+        label.textColor = .init(named: "SantaColor")
+        label.adjustsFontSizeToFitWidth = true
+        
+        return label
+    }()
+    
+    let line: UIView = {
+        let view = UIView()
+        view.backgroundColor = .init(named: "SantaColor")
+        
+        return view
+    }()
+    
+    let stack: UIStackView = {
+        let stack = UIStackView()
+        stack.axis = .vertical
+        
+        return stack
+    }()
+    
     func layout(data: LargeViewModel) {
-        self.subviews.forEach{$0.removeFromSuperview()}
-        let title: UILabel = UILabel()
         self.addSubview(title)
         title.text = data.title
-        title.font = .preferredFont(for: .body, weight: .bold)
-        title.numberOfLines = 0
-        title.adjustsFontSizeToFitWidth = true
-        title.textColor = .init(named: "SantaColor")
         title.translatesAutoresizingMaskIntoConstraints = false
         NSLayoutConstraint.activate([
             title.topAnchor.constraint(equalTo: self.topAnchor, constant: 10),
             title.leftAnchor.constraint(equalTo: self.leftAnchor, constant: 10)
         ])
         
-        let line = UIView()
         self.addSubview(line)
-        
-        line.backgroundColor = .init(named: "SantaColor")
         line.translatesAutoresizingMaskIntoConstraints = false
         NSLayoutConstraint.activate([
             line.heightAnchor.constraint(equalToConstant: 1),
@@ -37,36 +51,10 @@ class DetailCell: UICollectionViewCell {
             line.rightAnchor.constraint(equalTo: self.rightAnchor, constant: -15),
         ])
         
-        
-        
-        var contentLabels: [UILabel] = []
-        var contentTitleLabels: [UILabel] = []
-        
-        for content in data.contents {
-            let contentLabel = UILabel()
-            contentLabel.text = content.content
-            contentLabel.font = .preferredFont(for: .title1, weight: .bold)
-            contentLabel.numberOfLines = 0
-
-            let contentTitleLabel = UILabel()
-            contentTitleLabel.text = content.contentTitle
-            contentTitleLabel.font = .preferredFont(forTextStyle: .body)
-            contentTitleLabel.numberOfLines = 0
-            contentLabels.append(contentLabel)
-            contentTitleLabels.append(contentTitleLabel)
-        }
-        
-        let stack = UIStackView()
+        stack.subviews.forEach{ $0.removeFromSuperview() }
         self.addSubview(stack)
-        stack.axis = .vertical
-        
-        for index in 0..<data.contents.count {
-            let horizontalStack = UIStackView()
-            horizontalStack.axis = .horizontal
-            horizontalStack.distribution = .equalCentering
-            horizontalStack.addArrangedSubview(contentLabels[index])
-            horizontalStack.addArrangedSubview(contentTitleLabels[index])
-            stack.addArrangedSubview(horizontalStack)
+        for content in data.contents {
+            stack.addArrangedSubview(UIStackView(content: content))
         }
         
         stack.translatesAutoresizingMaskIntoConstraints = false
@@ -87,17 +75,24 @@ extension UIFont {
         return metrics.scaledFont(for: font)
     }
 }
-//extension DetailCell {
-//    override func draw(_ rect: CGRect) {
-//        let path = UIBezierPath()
-//        path.lineWidth = 1
-//        path.lineJoinStyle = .round
-//        let startingX: CGFloat = title.frame.origin.x + title.frame.width
-//        let startingY: CGFloat = title.frame.origin.y + title.frame.height / 2
-//        path.move(to: CGPoint(x: startingX, y: startingY))
-//        path.addLine(to: CGPoint(x: self.bounds.width, y: startingY))
-//        path.close()
-//        UIColor(named: "SantaColor")?.set()
-//        path.stroke()
-//    }
-//}
+
+extension UIStackView {
+    convenience init(content: CellContentEntity) {
+        self.init()
+        self.axis = .horizontal
+        self.distribution = .equalCentering
+        
+        let contentLabel = UILabel()
+        contentLabel.text = content.content
+        contentLabel.font = .preferredFont(for: .title1, weight: .bold)
+        contentLabel.numberOfLines = 0
+
+        let contentTitleLabel = UILabel()
+        contentTitleLabel.text = content.contentTitle
+        contentTitleLabel.font = .preferredFont(forTextStyle: .body)
+        contentTitleLabel.numberOfLines = 0
+        
+        self.addArrangedSubview(contentLabel)
+        self.addArrangedSubview(contentTitleLabel)
+    }
+}
